a0372daca16a59fd9dc60d61f5a70ed56ea082d6
Unknown <tukigase@gmail.com>
2018-04-27 11:44:45 +0900

    Remove redundant codes.

--- Form1.cs
+++ Form1.cs
@@ -15,21 +15,7 @@ namespace DWM_Memory_Leak
         public Form1()
         {
             InitializeComponent();
-
-            var screens = Screen.AllScreens;
-            var x = screens.Select(s => s.Bounds.X).Min();
-            var y = screens.Select(s => s.Bounds.Y).Min();
-            var w = screens.Select(s => s.Bounds.X + s.Bounds.Width).Max() - x;
-            var h = screens.Select(s => s.Bounds.Y + s.Bounds.Height).Max() - y;
-            this.Location = new Point(x, y);
-            this.Size = new Size(w, h);
-
-            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.TopMost = true;
-            this.TopLevel = true;
+            this.Size = new Size(1000, 1000);
             this.BackColor = Color.Black;
             this.TransparencyKey = Color.Black;
         }
